ccc80348caf87d149bb2cacc27a2a0fee71be68b
Jackyp101 <46499155+Jackyp101@users.noreply.github.com>
2019-01-09 23:14:43 +0000

    Assignment2

---
+++ js/js.js
@@ -0,0 +1,35 @@
+$('.scrollTo').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 500);
+    return false;
+});
+
+window.onload=function smoothScroll(target, duration) {
+    var target = document.querySelector(".last");
+    var targetPosition = target.getBoundingClientRect().top;
+    var startPosition = window.pageYOffset || window.scrollY;
+    var distance = targetPosition - startPosition;
+    var startTime = null;
+  
+    function loop(currentTime) {
+      if (startTime === null) startTime = currentTime;
+      var timeElapsed = currentTime - startTime;
+      var run = ease(timeElapsed, startPosition, distance, duration);
+      window.scrollTo(0, run);
+      if (timeElapsed < duration) requestAnimationFrame(loop);
+    }
+    function ease(t, b, c, d) {
+      t /= d / 2;
+      if (t < 1) return c / 2 * t * t + b;
+      t--;
+      return -c / 2 * (t * (t - 2) - 1) + b;
+    }
+    requestAnimationFrame(loop);
+  }
+
+  var scrollTo = document.querySelector('.scrollTo')
+
+  scrollTo.addEventListener('click',function(){
+    smoothScroll('#showcase2', 1000);
+  });
